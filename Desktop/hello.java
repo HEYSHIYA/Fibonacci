4f21c53919f9d2bf8e2c3f0a11377e85927f7582
HEYSHIYA <165045578+HEYSHIYA@users.noreply.github.com>
2024-11-19 20:24:46 +0530

    Update hello.java

--- Desktop/hello.java
+++ Desktop/hello.java
@@ -1,5 +1,25 @@
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+import java.util.Scanner;
+public class Fibo {
+    public static void main(String args[]){
+        
+        int count=10;
+        System.out.println("Fibonacci series:");
+        for (int i=0;i<count;i++){
+            System.out.print(fibonacci(i)+" ");
+        }
+
+        
+        
     }
+
+    public static int fibonacci(int i){
+    
+
+        if (i<=1){
+            return 1;
+        }
+
+        return fibonacci(i-1)+fibonacci(i-2);
+    }
+    
 }
